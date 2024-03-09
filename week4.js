e6b2b5e9b66f69b5ed5bd0dd2dd28a772d41cf9c
kholah26 <kholakhalid1995@gmail.com>
2024-03-09 17:02:30 +0000

    changing paragraph content

--- week4.js
+++ week4.js
@@ -3,6 +3,10 @@ function myLoadFunction(){
     var element = document.getElementById('pageheading');
 
     element.firstChild.nodeValue = 'new heading';
+
+    var element = document.getElementById('pageparagraph');
+
+    element.firstChild.nodeValue = 'new paragraph';
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
