98e86bba4bc39c62166756974f0d6e605f203d76
kholah26 <kholakhalid1995@gmail.com>
2024-03-24 18:41:18 +0000

    circle only moving left ( using keycode =37)

--- week5.js
+++ week5.js
@@ -1,6 +1,15 @@
 function ClickEvent(event) {
+    
+    if (event.keyCode==37) {
+  var element = document.getElementById('circle');
+  var PositionLeft =element.offsetLeft;
+  element.style.left = PositionLeft - 10 + 'px';
+    }
+  else 
+  {
     console.log(event.keyCode);
-  
+  }
+
 }
     function myLoadFunction(){
        
