9b90e44bbf020ff13a4f8a2a8f0249b19badb9e7
kholah26 <kholakhalid1995@gmail.com>
2024-03-24 18:59:50 +0000

    cmoving circle using set inteval

--- week5.js
+++ week5.js
@@ -1,27 +1,42 @@
 function ClickEvent(event) {
     
     if (event.keyCode==37) {
-  var element = document.getElementById('circle');
-  var PositionLeft =element.offsetLeft;
-  element.style.left = PositionLeft - 1 + 'px';
-    }
+        setInterval(moveDown,10);}
+
     if (event.keyCode==38) {
+        setInterval(moveUP,10);}
+        
+     if (event.keyCode==39) {
+            setInterval(moveLeft,10);}
+
+    if (event.keyCode==40) {setInterval(moveRight,10);}
+                
+
+}
+
+function moveRight(){
+    var element = document.getElementById('circle');
+    var PositionLeft =element.offsetLeft;
+    element.style.left = PositionLeft - 1 + 'px';
+      }
+    function moveUP(){
         var element = document.getElementById('circle');
         var PositionUp =element.offsetTop;
         element.style.top = PositionUp - 1 + 'px';
-          }
-          if (event.keyCode==39) {
+        }
+
+        function moveLeft(){
             var element = document.getElementById('circle');
             var PositionRight =element.offsetLeft;
             element.style.left = PositionRight + 1 + 'px';
               }
-              if (event.keyCode==40) {
+              function moveDown(){
                 var element = document.getElementById('circle');
                 var PositionDown =element.offsetTop;
                 element.style.top = PositionDown + 1 + 'px';
                   }
 
-}
+
     function myLoadFunction(){
        
         document.addEventListener('keydown',ClickEvent );
