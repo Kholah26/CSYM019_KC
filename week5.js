cea022d8d668cdaba1481f0900dc80f79722f097
kholah26 <kholakhalid1995@gmail.com>
2024-03-24 14:58:06 +0000

    opacity change from 0.5 to 1 for circle

--- week5.js
+++ week5.js
@@ -1,13 +1,13 @@
 function ClickEvent(){
 var element = document.getElementById('circle')
     element.style.backgroundColor = 'blue';
-    element.style.opacity = 0.5;
+    element.style.opacity = 1;
 
     }
     function myLoadFunction(){
         var element = document.getElementById('circle');
         element.addEventListener('click',ClickEvent );
-    
+        element.style.opacity = 0.5;
         
     }
     
