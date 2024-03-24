d14e4ee7d9c7ad9ab6b8344fdc7e716d99ff8adf
kholah26 <kholakhalid1995@gmail.com>
2024-03-24 18:53:01 +0000

    is statements to move circle up,down,right,left

--- week5.js
+++ week5.js
@@ -3,12 +3,23 @@ function ClickEvent(event) {
     if (event.keyCode==37) {
   var element = document.getElementById('circle');
   var PositionLeft =element.offsetLeft;
-  element.style.left = PositionLeft - 10 + 'px';
+  element.style.left = PositionLeft - 1 + 'px';
     }
-  else 
-  {
-    console.log(event.keyCode);
-  }
+    if (event.keyCode==38) {
+        var element = document.getElementById('circle');
+        var PositionUp =element.offsetTop;
+        element.style.top = PositionUp - 1 + 'px';
+          }
+          if (event.keyCode==39) {
+            var element = document.getElementById('circle');
+            var PositionRight =element.offsetLeft;
+            element.style.left = PositionRight + 1 + 'px';
+              }
+              if (event.keyCode==40) {
+                var element = document.getElementById('circle');
+                var PositionDown =element.offsetTop;
+                element.style.top = PositionDown + 1 + 'px';
+                  }
 
 }
     function myLoadFunction(){
